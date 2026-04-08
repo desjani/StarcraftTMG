032070b9c79aca0a25be6d67992aaa103001f999
desjani <jaredtritsch@gmail.com>
2026-04-08 16:47:30 -0600

    Fix aid toolbar overflow: single-column default, bump SW cache v3

--- web/public/mobile/sw.js
+++ web/public/mobile/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'adjutant-mobile-v2';
+const CACHE_VERSION = 'adjutant-mobile-v3';
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
